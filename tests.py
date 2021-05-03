4f72c702ed80601930a4b5e7a327a9a37e314d3e
Sian Molloy <sianmimo@gmail.com>
2021-05-03 16:43:35 -0600

    Refactor tests

--- tests.py
+++ tests.py
@@ -10,13 +10,17 @@ class TestCalcMaxProfits(unittest.TestCase):
         self.assertEqual(calculate_max_profit([7, 5, 6, 10, 7, 4, 5, 8]), 5)
         self.assertEqual(calculate_max_profit([12, 9, 8, 224, 72, 1006, 78]), 998)
         self.assertEqual(calculate_max_profit([10087, 2004, 756]), -1248)
-        #decending prices
+
+    def test_calc_max_profit_descending_prices(self):
         self.assertEqual(calculate_max_profit([10, 8, 5, 4, 3, 1]), -1)
-        #ascending prices
+    
+    def test_calc_max_profit_ascending_prices(self):
         self.assertEqual(calculate_max_profit([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), 9)        
-        #uniform prices
+
+    def test_calc_max_profit_unifrom_prices(self):
         self.assertEqual(calculate_max_profit([7, 7, 7, 7, 7]), 0)
-        #min number of prices
+        
+    def test_calc_max_profit_two_prices(self):
         self.assertEqual(calculate_max_profit([100, 9]), -91)
         self.assertEqual(calculate_max_profit([1, 895]), 894)
 
@@ -24,13 +28,17 @@ class TestCalcMaxProfits(unittest.TestCase):
         self.assertEqual(calculate_max_profit_with_short_sell([4, 7, 5, 9, 6, 2]), 7)
         self.assertEqual(calculate_max_profit_with_short_sell([2, 6, 3, 8, 5, 3]), 6)
         self.assertEqual(calculate_max_profit_with_short_sell([10087, 2004, 756]), 9331)
-        #decending prices
+    
+    def test_calc_max_profit_with_short_sell_descending_prices(self):
         self.assertEqual(calculate_max_profit_with_short_sell([10, 8, 5, 4, 3, 1]), 9)
-        #ascending prices
+    
+    def test_calc_max_profit_with_short_sell_ascending_prices(self):
         self.assertEqual(calculate_max_profit_with_short_sell([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), 9)        
-        #uniform prices
+        
+    def test_calc_max_profit_with_short_sell_unifrom_prices(self):
         self.assertEqual(calculate_max_profit_with_short_sell([7, 7, 7, 7, 7]), 0)
-        #min number of prices
+    
+    def test_calc_max_profit_with_short_sell_two_prices(self):
         self.assertEqual(calculate_max_profit_with_short_sell([1, 895]), 894)
         self.assertEqual(calculate_max_profit_with_short_sell([895, 1]), 894)
 
